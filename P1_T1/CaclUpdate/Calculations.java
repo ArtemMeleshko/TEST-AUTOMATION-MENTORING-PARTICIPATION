c11afbe3b139ef3a1f5e15a23310b9b5922a0632
ArtemMeleshko <artem50180@mail.ru>
2018-02-24 12:57:50 +0300

    Update Calculations.java

--- P1_T1/CaclUpdate/Calculations.java
+++ P1_T1/CaclUpdate/Calculations.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 
 public class Calculations {
-    int first, second, result;
+    Double first, second, result;
     String oper, YorN;
     String frst = "\nEnter a 1st number:";
     String sec = "\nEnter a 2nd number:";
@@ -10,63 +10,103 @@ public class Calculations {
     String zero = "\nYou can not divide it by zero";
     String repeat = "\nDo you want to repeat? Y/N";
     String thanks = "\nThank you for using this program.";
-    String incorrectOperation = "\nUnfortunately, our program can not perform this operation: ";
+    String incorrectOperation = "\nUnfortunately, our program can not perform this operation. ";
+    String notNumber = "\nThat not a number! Enter again";
+
 
     Scanner in = new Scanner(System.in);
 
+    private void firstValue() {
+        System.out.println(frst);
+
+        while (!in.hasNextDouble() && !in.hasNextInt()) {
+            System.out.println(notNumber);
+            in.next();
+        }
+
+        first = in.nextDouble();
+
+    }
+
+    private void secondValue() {
+        System.out.println(sec);
+
+        while (!in.hasNextDouble() && !in.hasNextInt()) {
+            System.out.println(notNumber);
+            in.next();
+        }
+
+        second = in.nextDouble();
+
+    }
+
+    private void oper() {
+        System.out.println(operation);
+        while (!in.hasNext("[+-/*]")) {
+            System.out.println(incorrectOperation + operation);
+            in.next();
+        }
+        oper = in.next();
+    }
+
+  /* private static String input(double first, double second) {
+            if(first == (long) first)
+                return String.format("%d",(long)first);
+            else
+                return String.format("%s",first);
+
+    }*/
+
+    private void repeatOrNot() {
+        System.out.println(repeat);
+        YorN = in.next();
+
+        if (YorN.equals("Y")) {
+            calculations();
+        } else
+            System.out.println(thanks);
+
+    }
+
+
     public void calculations() {
 
-        System.out.println(frst);
-        first = in.nextInt();
-        System.out.println(sec);
-        second = in.nextInt();
-
-        System.out.println(operation);
-        oper = in.next();
-
-        System.out.println("You entered: " + first + " " + oper + " " + second);
-        //System.out.printf("int1 = %d, int2 = %d", first, second);
+        firstValue();
+        oper();
+        secondValue();
+        //input(first,second);
 
         switch (oper) {
             case "+":
                 result = first + second;
                 System.out.println("\n" + first + " + " + second + " = " + result);
+                repeatOrNot();
                 break;
             case "-":
                 result = first - second;
                 System.out.println("\n" + first + " - " + second + " = " + result);
+                repeatOrNot();
                 break;
             case "*":
                 result = first * second;
                 System.out.println("\n" + first + " * " + second + " = " + result);
+                repeatOrNot();
                 break;
             case "/":
                 if (second == 0) {
-                    System.out.println(zero + repeat);
-                    YorN = in.next();
-
-                    if (YorN.equals("Y")) {
-                        calculations();
-                    } else
-                        System.out.println(thanks);
+                    System.out.println(zero);
+                    repeatOrNot();
                     break;
 
                 } else {
-                    result = first + second;
+                    result = first / second;
                     System.out.println("\n" + first + " / " + second + " = " + result);
+                    repeatOrNot();
                 }
-
-
                 break;
             default:
-                
-            System.out.println(incorrectOperation + oper + repeat);
-                YorN = in.next();
-
-                if (YorN.equals("Y")) {
-                    calculations();
-                } else
-                    System.out.println(thanks);
+                System.out.println(incorrectOperation + oper);
+                repeatOrNot();
                 break;
 
         }
@@ -74,4 +114,3 @@ public class Calculations {
 
     }
 }
-
