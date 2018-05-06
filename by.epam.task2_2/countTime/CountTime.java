28c5a8b29fe2255a1a93f920bc7dd594236c11f6
ArtemMeleshko <artem50180@mail.ru>
2018-05-06 20:48:33 +0300

    Create CountTime.java

---
+++ by.epam.task2_2/countTime/CountTime.java
@@ -0,0 +1,10 @@
+package by.epam.task2_2.countTime;
+
+public class CountTime {
+
+    protected long CountTime(long startTime, long endTime) {
+        long finishTime = endTime - startTime;
+        return finishTime;
+
+    }
+}
