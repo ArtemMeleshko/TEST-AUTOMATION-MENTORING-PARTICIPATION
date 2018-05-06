872e1e492b5f71a6805653a5e7776061abf74adb
ArtemMeleshko <artem50180@mail.ru>
2018-05-06 20:48:07 +0300

    Create IActions.java

---
+++ by.epam.task2_2/actions/IActions.java
@@ -0,0 +1,10 @@
+package by.epam.task2_2.actions;
+
+public interface IActions {
+    void add();
+    void print();
+    void removeElement(int elem0,int elem1,int elem2, int elem3);
+    void get(int element0, int element1, int element2, int element3);
+    void update(int index,int element);
+
+}
